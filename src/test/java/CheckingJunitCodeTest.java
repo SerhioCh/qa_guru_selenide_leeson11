9a815c95afe45be87c678c5746e8aafab52096fb
Сергей Квин <lorancem@mail.ru>
2022-11-22 23:05:27 +0300

    fix tests

---
+++ src/test/java/CheckingJunitCodeTest.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byLinkText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CheckingJunitCodeTest {
+
+    @Test
+   void checkingTextSoftAssertions() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".markdown-body").shouldHave(text("Soft assertions"));
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+
+    }
+}
